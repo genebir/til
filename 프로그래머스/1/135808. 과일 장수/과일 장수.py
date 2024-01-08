dfea1ab97d08e32ba6d23243a49a6f8721931db0
genebir <99165509+genebir@users.noreply.github.com>
2024-01-08 16:00:55 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,13 +1,1 @@
-def solution(k, m, score):
-    answer=0
-    tmp=[0]*(k+1)
-    for _ in score:
-        tmp[_]+=1
-    tmp_box=[]
-    for i,v in enumerate(tmp[::-1]):
-        for _ in range(v):
-            tmp_box.append(k-i)
-            if len(tmp_box)==m:
-                answer+=min(tmp_box)*m
-                tmp_box=[]
-    return answer
+solution = lambda _, m, s: sum(sorted(s)[-m::-m]) * m
