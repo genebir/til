34893f25195aaf39f4707c0e8b8475d40dd5c219
genebir <99165509+genebir@users.noreply.github.com>
2024-01-09 22:18:04 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,16 @@
+def solution(s):
+    d={
+        '0':'zero',
+        '1':'one',
+        '2':'two',
+        '3':'three',
+        '4':'four',
+        '5':'five',
+        '6':'six',
+        '7':'seven',
+        '8':'eight',
+        '9':'nine'
+    }
+    for k,v in d.items():
+        s=s.replace(v,k)
+    return int(s)
